7954c03d9b0370fc5ec01d7f0ff2aba08786d77d
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-10-13 09:35:12 -0300

    Remove teste para pasta source

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -20,11 +20,3 @@ def test_venv_folder():
 def test_gitignore_file(expected):
     if not setup.project_name() == 'python-project-template':
         assert os.path.exists(expected) == True, f'{expected} must exist.'
-
-
-def test_source_code_folder():
-    if not setup.project_name() == 'python-project-template':
-        underscore_project_name = project_name.replace('-', '_')
-        assert (
-            os.path.exists(underscore_project_name) == True
-        ), 'source folder must exist.'
